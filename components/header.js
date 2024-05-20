541a83ab2d1169511b3b9a87aa49b880c4269ce6
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-05-20 11:33:17 +0300

    Link ile a arasindaki farki anlamak icin counter yapildi. nav bar koyuldu

--- components/header.js
+++ components/header.js
@@ -1,5 +1,14 @@
+import Counter from "@/components/counter";
+import Link from "next/link";
+
 export default function  Header(){
     return(<header className="p-20 border border-amber-600">
-        Im learning
-    </header>)
+        <ul className="flex space-x-4">
+            <li><Link href="/">Home</Link></li>
+            <li><Link href="/about">About</Link></li>
+            <li><Link href="/about/projects">Projects</Link></li>
+        </ul>
+            <Counter/>
+    </header>
+    )
 }
