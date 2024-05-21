0f1e4f2042378037d0ba0261e496b4e9d1d49595
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-05-21 10:52:01 +0300

    cok basit fetch ornegi

--- app/about/projects/page.js
+++ app/about/projects/page.js
@@ -1,5 +1,21 @@
-export default function ProjectsPage() {
+export default async function ProjectsPage() {
+
+    const response = await fetch('http://localhost:3001/repos');
+
+    const repos = await response.json();
+
+
     return (<>
-    Projects burasii bu gozukmemesi lazim cache ve prefetch denemeleri yapiyorum.
+        <div className="p-20">
+            <h1 className="mb-8 text-xl">Projects</h1>
+            <ul>
+                {repos.map(repo => (
+                    <li key={repo.id} className="mb-4">
+                        <div>{repo.title}</div>
+                        <div>{repo.description}</div>
+                        <div>{repo.stargazers_count}</div>
+                    </li>))}
+            </ul>
+        </div>
     </>)
 }
