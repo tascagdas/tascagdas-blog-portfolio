f905859322e6828297765137df5ddd34ce3d400f
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-06-03 22:15:45 +0300

    apos hatası giderildi. build hatası için

--- app/about/page.js
+++ app/about/page.js
@@ -11,11 +11,11 @@ export default function AboutPage() {
         <h1 className="text-3xl mb-8 underline">Hakkımda</h1>
         <p className="mb-8">
 
-            Merhaba, ben Çağdaş Taş. Eğitim ve kariyer yolculuğum boyunca çeşitli deneyimlerle donanmış, dinamik ve tutkulu bir profesyonelim. Lise eğitimimi Tarhan Koleji'nde tamamladım. Lise yıllarımda iki yaz boyunca İngiltere'de dil eğitimi alarak İngilizcemi geliştirdim.
+            Merhaba, ben Çağdaş Taş. Eğitim ve kariyer yolculuğum boyunca çeşitli deneyimlerle donanmış, dinamik ve tutkulu bir profesyonelim. Lise eğitimimi Tarhan Koleji&apos;nde tamamladım. Lise yıllarımda iki yaz boyunca İngiltere&apos;de dil eğitimi alarak İngilizcemi geliştirdim.
 
-            Üniversite eğitimime Yıldız Teknik Üniversitesi Kimya Bölümü'nde başladım ve son senemde kimya mühendisliğinin benim için daha uygun olduğuna karar vererek Eskişehir Osmangazi Üniversitesi Kimya Mühendisliği Bölümü'ne geçiş yaptım ve lisans eğitimimi burada tamamladım. Ardından aynı üniversitede Proses ve Reaktör Tasarımı Anabilim Dalı'nda yüksek lisansa başladım.
+            Üniversite eğitimime Yıldız Teknik Üniversitesi Kimya Bölümü&apos;nde başladım ve son senemde kimya mühendisliğinin benim için daha uygun olduğuna karar vererek Eskişehir Osmangazi Üniversitesi Kimya Mühendisliği Bölümü&apos;ne geçiş yaptım ve lisans eğitimimi burada tamamladım. Ardından aynı üniversitede Proses ve Reaktör Tasarımı Anabilim Dalı&apos;nda yüksek lisansa başladım.
 
-            Profesyonel kariyerime ise Kütahya'da İGSAŞ'ta Asit Üretim Mühendisi olarak adım attım. İstanbul'a dönüş yaptıktan sonra, yazılımın benim için uygun bir kariyer olacağına karar verdim. Bu doğrultuda Bahçeşehir Üniversitesi'nin düzenlediği 600 saatlik yüz yüze eğitim ve 200+ saatlik online derslerle Fullstack Web Developer eğitimini tamamladım.
+            Profesyonel kariyerime ise Kütahya&apos;da İGSAŞ&apos;ta Asit Üretim Mühendisi olarak adım attım. İstanbul&apos;a dönüş yaptıktan sonra, yazılımın benim için uygun bir kariyer olacağına karar verdim. Bu doğrultuda Bahçeşehir Üniversitesi&apos;nin düzenlediği 600 saatlik yüz yüze eğitim ve 200+ saatlik online derslerle Fullstack Web Developer eğitimini tamamladım.
 
             Hem mühendislik hem de yazılım alanındaki bilgi ve deneyimlerimle, sürekli öğrenme ve gelişme isteğiyle projelerimi hayata geçirmeye devam ediyorum.
         </p>
